8af54df251256147a250fc37a19e09e533a34f36
SjurGust <sjurandreaslundstein@gmail.com>
2015-08-25 10:26:30 +0200

    cleaning up

--- test_dropbox_api.rb
+++
@@ -1,7 +0,0 @@
-require 'dropbox_sdk'
-
-client = DropboxClient.new("mk6zdX6kkmoAAAAAAAAAAZpOH-rmcRAsoqmWSEg2jmA")
-
-#client.put_file('/xmltes2t.rb', open('./xmltes2t.rb'))
-
-puts "linked account:", client.account_info().inspect
--- test_google/js/metabucket.config.js
+++
@@ -1,2 +0,0 @@
-var GCS_API_KEY = 'AIzaSyCAu4PR5qcee6HXMggrVU4bW995ZEAwJ98';
-var GCS_BUCKET = 'bucket_sjur';
